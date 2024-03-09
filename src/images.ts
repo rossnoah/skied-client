9db9cb387685d7699f653a3acf1a2372f5034dcb
Noah Ross <me@noah.dev>
2024-03-09 22:27:25 +0100

    update images and skiarea list

--- src/images.ts
+++ src/images.ts
@@ -1,79 +1,77 @@
 // Static imports for logos
+import Alta from "../assets/logos/alta.png";
+import ArapahoeBasin from "../assets/logos/arapahoebasin.png";
+import AspenLogo from "../assets/logos/aspensnowmass.png";
+import BeaverCreek from "../assets/logos/beavercreek.png";
+import Boreal from "../assets/logos/boreal.png";
+import Breckenridge from "../assets/logos/breckenridge.png";
+import Chamonix from "../assets/logos/chamonix.png";
+import CopperMountain from "../assets/logos/coppermountain.png";
+import CrestedButte from "../assets/logos/crestedbutte.png";
+import DeerValley from "../assets/logos/deervalley.png";
 import DefaultLogo from "../assets/logos/default.png";
-import ArapahoeBasin from "../assets/logos/ArapahoeBasin.jpeg";
-import AspenLogo from "../assets/logos/AspenSnowmass.png";
-import Breckenridge from "../assets/logos/Breckenridge.png";
-import BeaverCreek from "../assets/logos/BeaverCreek.png";
-import CopperMountain from "../assets/logos/CopperMountain.png";
-import CrestedButte from "../assets/logos/CrestedButte.png";
-import EchoMountain from "../assets/logos/EchoMountain.jpeg";
-import Eldora from "../assets/logos/Eldora.png";
-import Hesperus from "../assets/logos/Hesperus.png";
-import Silverton from "../assets/logos/Silverton.png";
-import PalisadesTahoe from "../assets/logos/PalisadesTahoe.png";
-import Snowbird from "../assets/logos/Snowbird.jpeg";
-import Solitude from "../assets/logos/Solituide.png";
-import Mammoth from "../assets/logos/Mammoth.png";
-import ParkCity from "../assets/logos/ParkCity.png";
-import DeerValley from "../assets/logos/DeerValley.png";
-import Alta from "../assets/logos/Alta.png";
-import Vail from "../assets/logos/Vail.png";
-import Telluride from "../assets/logos/Telluride.png";
-import SkiCooper from "../assets/logos/SkiCooper.png";
-import Steamboat from "../assets/logos/Steamboat.png";
-import SugarBowl from "../assets/logos/SugarBowl.jpeg";
-import Heavenly from "../assets/logos/Heavenly.png";
-import Boreal from "../assets/logos/Boreal.png";
-import DonnerSkiRanch from "../assets/logos/DonnerSkiRanch.jpeg";
-import Homewood from "../assets/logos/Homewood.jpeg";
-import Kirkwood from "../assets/logos/Kirkwood.png";
-import Northstar from "../assets/logos/Northstar.jpeg";
-import TahoeDonner from "../assets/logos/TahoeDonner.jpeg";
-import Zermatt from "../assets/logos/Zermatt.jpeg";
-import Verbier from "../assets/logos/Verbier.jpg";
-import StMoritz from "../assets/logos/StMoritz.png";
-import Megeve from "../assets/logos/Megeve.jpeg";
-import Chamonix from "../assets/logos/Chamonix.png";
-import Kitzbuhel from "../assets/logos/Kitzbuhel.png";
+import DonnerSkiRanch from "../assets/logos/donnerskiranch.png";
+import EchoMountain from "../assets/logos/echomountain.png";
+import Eldora from "../assets/logos/eldora.png";
+import Heavenly from "../assets/logos/heavenly.png";
+import Hesperus from "../assets/logos/hesperus.png";
+import Homewood from "../assets/logos/homewood.png";
+import Kirkwood from "../assets/logos/kirkwood.png";
+import Kitzbuhel from "../assets/logos/kitzbuhel.png";
+import Mammoth from "../assets/logos/mammoth.png";
+import Megeve from "../assets/logos/megeve.png";
+import Northstar from "../assets/logos/northstar.png";
+import PalisadesTahoe from "../assets/logos/palisadestahoe.png";
+import ParkCity from "../assets/logos/parkcity.png";
+import Silverton from "../assets/logos/silverton.png";
+import SkiCooper from "../assets/logos/skicooper.png";
+import Solitude from "../assets/logos/solituide.png";
+import Snowbird from "../assets/logos/snowbird.png";
+import StMoritz from "../assets/logos/stmoritz.png";
+import Steamboat from "../assets/logos/steamboat.png";
+import SugarBowl from "../assets/logos/sugarbowl.png";
+import TahoeDonner from "../assets/logos/tahoedonner.png";
+import Telluride from "../assets/logos/telluride.png";
+import Verbier from "../assets/logos/verbier.png";
+import Vail from "../assets/logos/vail.png";
+import Zermatt from "../assets/logos/zermatt.png";
 
 // Mapping ski area names to logos
 export const logos = {
+  alta: Alta,
+  arapahoebasin: ArapahoeBasin,
+  aspensnowmass: AspenLogo,
+  beavercreek: BeaverCreek,
+  boreal: Boreal,
+  breckenridge: Breckenridge,
+  chamonix: Chamonix,
+  coppermountain: CopperMountain,
+  crestedbutte: CrestedButte,
+  deervalley: DeerValley,
   default: DefaultLogo,
-  "Arapahoe Basin": ArapahoeBasin,
-  "Aspen Snowmass": AspenLogo,
-  Breckenridge: Breckenridge,
-  "Copper Mountain": CopperMountain,
-  "Beaver Creek": BeaverCreek,
-  "Crested Butte": CrestedButte,
-  "Echo Mountain": EchoMountain,
-  Eldora: Eldora,
-  "Hesperus Ski Area": Hesperus,
-  "Silverton Mountain": Silverton,
-  "Palisades Tahoe": PalisadesTahoe,
-  Snowbird: Snowbird,
-  Solitude: Solitude,
-  "Mammoth Mountain": Mammoth,
-  "Park City Mountain": ParkCity,
-  "Deer Valley": DeerValley,
-  Alta: Alta,
-  Vail: Vail,
-  Telluride: Telluride,
-  "Ski Cooper": SkiCooper,
-  Steamboat: Steamboat,
-  "Sugar Bowl": SugarBowl,
-  "Heavenly Mountain Resort": Heavenly,
-  Boreal: Boreal,
-  "Donner Ski Ranch": DonnerSkiRanch,
-  Homewood: Homewood,
-  Kirkwood: Kirkwood,
-  "Northstar at Tahoe": Northstar,
-  "Tahoe Donner": TahoeDonner,
-  Zermatt: Zermatt,
-  Verbier: Verbier,
-  "St. Moritz": StMoritz,
-  "Meg\u00e8ve": Megeve,
-  Chamonix: Chamonix,
-  Kitzbuhel: Kitzbuhel,
-
-  // Add more mappings as needed
+  donnerskiranch: DonnerSkiRanch,
+  echomountain: EchoMountain,
+  eldora: Eldora,
+  heavenly: Heavenly,
+  hesperus: Hesperus,
+  homewood: Homewood,
+  kirkwood: Kirkwood,
+  kitzbuhel: Kitzbuhel,
+  mammoth: Mammoth,
+  megeve: Megeve,
+  northstarattahoe: Northstar,
+  palisadestahoe: PalisadesTahoe,
+  parkcitymountain: ParkCity,
+  silvertonmountain: Silverton,
+  skicooper: SkiCooper,
+  solitude: Solitude,
+  snowbird: Snowbird,
+  stmoritz: StMoritz,
+  steamboat: Steamboat,
+  sugarbowl: SugarBowl,
+  tahoedonner: TahoeDonner,
+  telluride: Telluride,
+  verbier: Verbier,
+  vail: Vail,
+  zermatt: Zermatt,
 };
