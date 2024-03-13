1e64807ef3f2fc5761cec965b5c9143f23eb93f8
Noah Ross <me@noah.dev>
2024-03-13 02:25:34 +0100

    ski area / image changes

--- src/images.ts
+++ src/images.ts
@@ -1,77 +1,457 @@
-// Static imports for logos
-import Alta from "../assets/logos/alta.png";
-import ArapahoeBasin from "../assets/logos/arapahoebasin.png";
-import AspenLogo from "../assets/logos/aspensnowmass.png";
-import BeaverCreek from "../assets/logos/beavercreek.png";
-import Boreal from "../assets/logos/boreal.png";
-import Breckenridge from "../assets/logos/breckenridge.png";
-import Chamonix from "../assets/logos/chamonix.png";
-import CopperMountain from "../assets/logos/coppermountain.png";
-import CrestedButte from "../assets/logos/crestedbutte.png";
-import DeerValley from "../assets/logos/deervalley.png";
-import DefaultLogo from "../assets/logos/default.png";
-import DonnerSkiRanch from "../assets/logos/donnerskiranch.png";
-import EchoMountain from "../assets/logos/echomountain.png";
-import Eldora from "../assets/logos/eldora.png";
-import Heavenly from "../assets/logos/heavenly.png";
-import Hesperus from "../assets/logos/hesperus.png";
-import Homewood from "../assets/logos/homewood.png";
-import Kirkwood from "../assets/logos/kirkwood.png";
-import Kitzbuhel from "../assets/logos/kitzbuhel.png";
-import Mammoth from "../assets/logos/mammoth.png";
-import Megeve from "../assets/logos/megeve.png";
-import Northstar from "../assets/logos/northstar.png";
-import PalisadesTahoe from "../assets/logos/palisadestahoe.png";
-import ParkCity from "../assets/logos/parkcity.png";
-import Silverton from "../assets/logos/silverton.png";
-import SkiCooper from "../assets/logos/skicooper.png";
-import Solitude from "../assets/logos/solituide.png";
-import Snowbird from "../assets/logos/snowbird.png";
-import StMoritz from "../assets/logos/stmoritz.png";
-import Steamboat from "../assets/logos/steamboat.png";
-import SugarBowl from "../assets/logos/sugarbowl.png";
-import TahoeDonner from "../assets/logos/tahoedonner.png";
-import Telluride from "../assets/logos/telluride.png";
-import Verbier from "../assets/logos/verbier.png";
-import Vail from "../assets/logos/vail.png";
-import Zermatt from "../assets/logos/zermatt.png";
+import Default from "../assets/logos/default.png";
+import aftonalps from "../assets/logos/aftonalps.png";
+import haystackmountain from "../assets/logos/haystackmountain.png";
+import scalymountain from "../assets/logos/scalymountain.png";
+import alpental from "../assets/logos/alpental.png";
+import heavenly from "../assets/logos/heavenly.png";
+import schweitzermountain from "../assets/logos/schweitzermountain.png";
+import alpinemountain from "../assets/logos/alpinemountain.png";
+import hesperus from "../assets/logos/hesperus.png";
+import sevenspringsmountain from "../assets/logos/sevenspringsmountain.png";
+import alpinevalley from "../assets/logos/alpinevalley.png";
+import hiddenvalley from "../assets/logos/hiddenvalley.png";
+import shasta from "../assets/logos/shasta.png";
+import alta from "../assets/logos/alta.png";
+import highlandsharbor from "../assets/logos/highlandsharbor.png";
+import shawneemountain from "../assets/logos/shawneemountain.png";
+import altasierra from "../assets/logos/altasierra.png";
+import hogadon from "../assets/logos/hogadon.png";
+import sierra from "../assets/logos/sierra.png";
+import angelfire from "../assets/logos/angelfire.png";
+import holidayvalley from "../assets/logos/holidayvalley.png";
+import silvermountain from "../assets/logos/silvermountain.png";
+import anthonylakes from "../assets/logos/anthonylakes.png";
+import homewood from "../assets/logos/homewood.png";
+import silverton from "../assets/logos/silverton.png";
+import appalachianskimountain from "../assets/logos/appalachianskimountain.png";
+import hoodoo from "../assets/logos/hoodoo.png";
+import skeetawk from "../assets/logos/skeetawk.png";
+import arapahoe from "../assets/logos/arapahoe.png";
+import hunter from "../assets/logos/hunter.png";
+import skiapache from "../assets/logos/skiapache.png";
+import aspensnowmass from "../assets/logos/aspensnowmass.png";
+import hurricaneridge from "../assets/logos/hurricaneridge.png";
+import skibradford from "../assets/logos/skibradford.png";
+import attitashmountain from "../assets/logos/attitashmountain.png";
+import hylandhills from "../assets/logos/hylandhills.png";
+import skibrule from "../assets/logos/skibrule.png";
+import badger from "../assets/logos/badger.png";
+import jackfrostbigboulder from "../assets/logos/jackfrostbigboulder.png";
+import skibutternut from "../assets/logos/skibutternut.png";
+import baldmountain from "../assets/logos/baldmountain.png";
+import jacksonhole from "../assets/logos/jacksonhole.png";
+import skimuseumofmaine from "../assets/logos/skimuseumofmaine.png";
+import baldy from "../assets/logos/baldy.png";
+import jiminypeak from "../assets/logos/jiminypeak.png";
+import skisantafe from "../assets/logos/skisantafe.png";
+import bear from "../assets/logos/bear.png";
+import june from "../assets/logos/june.png";
+import skiward from "../assets/logos/skiward.png";
+import beartoothbasin from "../assets/logos/beartoothbasin.png";
+import keystone from "../assets/logos/keystone.png";
+import smugglersnotch from "../assets/logos/smugglersnotch.png";
+import bearvalley from "../assets/logos/bearvalley.png";
+import killington from "../assets/logos/killington.png";
+import snowbasin from "../assets/logos/snowbasin.png";
+import beavercreek from "../assets/logos/beavercreek.png";
+import kingpine from "../assets/logos/kingpine.png";
+import snowbird from "../assets/logos/snowbird.png";
+import beavermountain from "../assets/logos/beavermountain.png";
+import kirkwood from "../assets/logos/kirkwood.png";
+import snowking from "../assets/logos/snowking.png";
+import belleayre from "../assets/logos/belleayre.png";
+import kitzbuhel from "../assets/logos/kitzbuhel.png";
+import snowyrange from "../assets/logos/snowyrange.png";
+import berkshireeastresort from "../assets/logos/berkshireeastresort.png";
+import littleswitzerland from "../assets/logos/littleswitzerland.png";
+import sodasprings from "../assets/logos/sodasprings.png";
+import bigrock from "../assets/logos/bigrock.png";
+import lookoutpass from "../assets/logos/lookoutpass.png";
+import soldierhollow from "../assets/logos/soldierhollow.png";
+import blackmountain from "../assets/logos/blackmountain.png";
+import loonmountain from "../assets/logos/loonmountain.png";
+import soldiermountain from "../assets/logos/soldiermountain.png";
+import blackmountainofmaine from "../assets/logos/blackmountainofmaine.png";
+import bluemountain from "../assets/logos/bluemountain.png";
+import boltonvalley from "../assets/logos/boltonvalley.png";
+import boreal from "../assets/logos/boreal.png";
+import bousquetmountain from "../assets/logos/bousquetmountain.png";
+import boynemountain from "../assets/logos/boynemountain.png";
+import breckenridge from "../assets/logos/breckenridge.png";
+import brettonwoods from "../assets/logos/brettonwoods.png";
+import brianhead from "../assets/logos/brianhead.png";
+import brighton from "../assets/logos/brighton.png";
+import bristol from "../assets/logos/bristol.png";
+import bromley from "../assets/logos/bromley.png";
+import buckhill from "../assets/logos/buckhill.png";
+import burkemountain from "../assets/logos/burkemountain.png";
+import camdensnowbowl from "../assets/logos/camdensnowbowl.png";
+import camelbackmountain from "../assets/logos/camelbackmountain.png";
+import cannonmountain from "../assets/logos/cannonmountain.png";
+import cascademountain from "../assets/logos/cascademountain.png";
+import catamount from "../assets/logos/catamount.png";
+import chamonix from "../assets/logos/chamonix.png";
+import cherrypeak from "../assets/logos/cherrypeak.png";
+import china from "../assets/logos/china.png";
+import christiemountain from "../assets/logos/christiemountain.png";
+import cooper from "../assets/logos/cooper.png";
+import cooperspur from "../assets/logos/cooperspur.png";
+import copper from "../assets/logos/copper.png";
+import copperpeak from "../assets/logos/copperpeak.png";
+import corbetscouloir from "../assets/logos/corbetscouloir.png";
+import cranmoremountain from "../assets/logos/cranmoremountain.png";
+import crestedbutte from "../assets/logos/crestedbutte.png";
+import crotchedmountain from "../assets/logos/crotchedmountain.png";
+import crystalmountain from "../assets/logos/crystalmountain.png";
+import crystalmtn from "../assets/logos/crystalmtn.png";
+import dartmouthskiway from "../assets/logos/dartmouthskiway.png";
+import deervalley from "../assets/logos/deervalley.png";
+import dodge from "../assets/logos/dodge.png";
+import dollarmountain from "../assets/logos/dollarmountain.png";
+import donner from "../assets/logos/donner.png";
+import eaglecrest from "../assets/logos/eaglecrest.png";
+import easton from "../assets/logos/easton.png";
+import echo from "../assets/logos/echo.png";
+import echovalley from "../assets/logos/echovalley.png";
+import eldora from "../assets/logos/eldora.png";
+import eyak from "../assets/logos/eyak.png";
+import giantsridge from "../assets/logos/giantsridge.png";
+import gore from "../assets/logos/gore.png";
+import granbyranch from "../assets/logos/granbyranch.png";
+import grandtarghee from "../assets/logos/grandtarghee.png";
+import granitegorge from "../assets/logos/granitegorge.png";
+import granitepeak from "../assets/logos/granitepeak.png";
+import gunstockmountain from "../assets/logos/gunstockmountain.png";
+import harrishill from "../assets/logos/harrishill.png";
+import losttrail from "../assets/logos/losttrail.png";
+import louploup from "../assets/logos/louploup.png";
+import loveland from "../assets/logos/loveland.png";
+import lutsenmountains from "../assets/logos/lutsenmountains.png";
+import madriverglen from "../assets/logos/madriverglen.png";
+import magicmountain from "../assets/logos/magicmountain.png";
+import mammoth from "../assets/logos/mammoth.png";
+import marquettemtn from "../assets/logos/marquettemtn.png";
+import meanylodge from "../assets/logos/meanylodge.png";
+import megeve from "../assets/logos/megeve.png";
+import middleburysnowbowl from "../assets/logos/middleburysnowbowl.png";
+import missionridge from "../assets/logos/missionridge.png";
+import monarch from "../assets/logos/monarch.png";
+import moose from "../assets/logos/moose.png";
+import moritz from "../assets/logos/moritz.png";
+import mountbachelor from "../assets/logos/mountbachelor.png";
+import mountkato from "../assets/logos/mountkato.png";
+import mountpleasantedinboro from "../assets/logos/mountpleasantedinboro.png";
+import mountrose from "../assets/logos/mountrose.png";
+import mountsnow from "../assets/logos/mountsnow.png";
+import mountsunapee from "../assets/logos/mountsunapee.png";
+import mtbaker from "../assets/logos/mtbaker.png";
+import mtbohemia from "../assets/logos/mtbohemia.png";
+import mtbrighton from "../assets/logos/mtbrighton.png";
+import mthoodmeadows from "../assets/logos/mthoodmeadows.png";
+import mthoodskibowl from "../assets/logos/mthoodskibowl.png";
+import nordicvalley from "../assets/logos/nordicvalley.png";
+import northstar from "../assets/logos/northstar.png";
+import nubsnob from "../assets/logos/nubsnob.png";
+import okemo from "../assets/logos/okemo.png";
+import otisridge from "../assets/logos/otisridge.png";
+import pajaritomountain from "../assets/logos/pajaritomountain.png";
+import palisadestahoe from "../assets/logos/palisadestahoe.png";
+import parkcity from "../assets/logos/parkcity.png";
+import patspeak from "../assets/logos/patspeak.png";
+import pebblecreek from "../assets/logos/pebblecreek.png";
+import peek from "../assets/logos/peek.png";
+import peter from "../assets/logos/peter.png";
+import pico from "../assets/logos/pico.png";
+import pinemtnjump from "../assets/logos/pinemtnjump.png";
+import plattekill from "../assets/logos/plattekill.png";
+import pleasantmountain from "../assets/logos/pleasantmountain.png";
+import pomerelle from "../assets/logos/pomerelle.png";
+import powder from "../assets/logos/powder.png";
+import powderhorn from "../assets/logos/powderhorn.png";
+import purgatory from "../assets/logos/purgatory.png";
+import raggedmountain from "../assets/logos/raggedmountain.png";
+import redriver from "../assets/logos/redriver.png";
+import roundtopmountain from "../assets/logos/roundtopmountain.png";
+import saddlebackmaine from "../assets/logos/saddlebackmaine.png";
+import sandiapeak from "../assets/logos/sandiapeak.png";
+import saskadenasix from "../assets/logos/saskadenasix.png";
+import solitude from "../assets/logos/solitude.png";
+import spiritmountain from "../assets/logos/spiritmountain.png";
+import steamboat from "../assets/logos/steamboat.png";
+import stevenspass from "../assets/logos/stevenspass.png";
+import stowe from "../assets/logos/stowe.png";
+import stratton from "../assets/logos/stratton.png";
+import sugarbowl from "../assets/logos/sugarbowl.png";
+import sugarbush from "../assets/logos/sugarbush.png";
+import sugarloaf from "../assets/logos/sugarloaf.png";
+import summit from "../assets/logos/summit.png";
+import summitsnoqualmie from "../assets/logos/summitsnoqualmie.png";
+import sundance from "../assets/logos/sundance.png";
+import sundayriver from "../assets/logos/sundayriver.png";
+import sunlight from "../assets/logos/sunlight.png";
+import sunvalley from "../assets/logos/sunvalley.png";
+import tahoedonner from "../assets/logos/tahoedonner.png";
+import tamarackresort from "../assets/logos/tamarackresort.png";
+import tamarackskiarea from "../assets/logos/tamarackskiarea.png";
+import taosskivalley from "../assets/logos/taosskivalley.png";
+import telluride from "../assets/logos/telluride.png";
+import thunder from "../assets/logos/thunder.png";
+import timberlinelodge from "../assets/logos/timberlinelodge.png";
+import timberridge from "../assets/logos/timberridge.png";
+import titcombmountain from "../assets/logos/titcombmountain.png";
+import titus from "../assets/logos/titus.png";
+import trappfamily from "../assets/logos/trappfamily.png";
+import trollhaugen from "../assets/logos/trollhaugen.png";
+import tusseymountain from "../assets/logos/tusseymountain.png";
+import tuxedo from "../assets/logos/tuxedo.png";
+import tyrolbasin from "../assets/logos/tyrolbasin.png";
+import vail from "../assets/logos/vail.png";
+import valley from "../assets/logos/valley.png";
+import verbier from "../assets/logos/verbier.png";
+import watervillevalley from "../assets/logos/watervillevalley.png";
+import welchvillage from "../assets/logos/welchvillage.png";
+import west from "../assets/logos/west.png";
+import whaleback from "../assets/logos/whaleback.png";
+import whiteface from "../assets/logos/whiteface.png";
+import whitepass from "../assets/logos/whitepass.png";
+import whitepine from "../assets/logos/whitepine.png";
+import whitetailski from "../assets/logos/whitetailski.png";
+import wildcatmountain from "../assets/logos/wildcatmountain.png";
+import wildmountain from "../assets/logos/wildmountain.png";
+import willamettepass from "../assets/logos/willamettepass.png";
+import wilmotmountain from "../assets/logos/wilmotmountain.png";
+import windham from "../assets/logos/windham.png";
+import winterpark from "../assets/logos/winterpark.png";
+import wolfcreek from "../assets/logos/wolfcreek.png";
+import wolfridgeski from "../assets/logos/wolfridgeski.png";
+import zermatt from "../assets/logos/zermatt.png";
 
-// Mapping ski area names to logos
 export const logos = {
-  alta: Alta,
-  arapahoebasin: ArapahoeBasin,
-  aspensnowmass: AspenLogo,
-  beavercreek: BeaverCreek,
-  boreal: Boreal,
-  breckenridge: Breckenridge,
-  chamonix: Chamonix,
-  coppermountain: CopperMountain,
-  crestedbutte: CrestedButte,
-  deervalley: DeerValley,
-  default: DefaultLogo,
-  donnerskiranch: DonnerSkiRanch,
-  echomountain: EchoMountain,
-  eldora: Eldora,
-  heavenly: Heavenly,
-  hesperus: Hesperus,
-  homewood: Homewood,
-  kirkwood: Kirkwood,
-  kitzbuhel: Kitzbuhel,
-  mammoth: Mammoth,
-  megeve: Megeve,
-  northstarattahoe: Northstar,
-  palisadestahoe: PalisadesTahoe,
-  parkcitymountain: ParkCity,
-  silvertonmountain: Silverton,
-  skicooper: SkiCooper,
-  solitude: Solitude,
-  snowbird: Snowbird,
-  stmoritz: StMoritz,
-  steamboat: Steamboat,
-  sugarbowl: SugarBowl,
-  tahoedonner: TahoeDonner,
-  telluride: Telluride,
-  verbier: Verbier,
-  vail: Vail,
-  zermatt: Zermatt,
+  default: Default,
+  aftonalps,
+  haystackmountain,
+  scalymountain,
+  alpental,
+  heavenly,
+  schweitzermountain,
+  alpinemountain,
+  hesperus,
+  sevenspringsmountain,
+  alpinevalley,
+  hiddenvalley,
+  shasta,
+  alta,
+  highlandsharbor,
+  shawneemountain,
+  altasierra,
+  hogadon,
+  sierra,
+  angelfire,
+  holidayvalley,
+  silvermountain,
+  anthonylakes,
+  homewood,
+  silverton,
+  appalachianskimountain,
+  hoodoo,
+  skeetawk,
+  arapahoe,
+  hunter,
+  skiapache,
+  aspensnowmass,
+  hurricaneridge,
+  skibradford,
+  attitashmountain,
+  hylandhills,
+  skibrule,
+  badger,
+  jackfrostbigboulder,
+  skibutternut,
+  baldmountain,
+  jacksonhole,
+  skimuseumofmaine,
+  baldy,
+  jiminypeak,
+  skisantafe,
+  bear,
+  june,
+  skiward,
+  beartoothbasin,
+  keystone,
+  smugglersnotch,
+  bearvalley,
+  killington,
+  snowbasin,
+  beavercreek,
+  kingpine,
+  snowbird,
+  beavermountain,
+  kirkwood,
+  snowking,
+  belleayre,
+  kitzbuhel,
+  snowyrange,
+  berkshireeastresort,
+  littleswitzerland,
+  sodasprings,
+  bigrock,
+  lookoutpass,
+  soldierhollow,
+  blackmountain,
+  loonmountain,
+  soldiermountain,
+  blackmountainofmaine,
+  bluemountain,
+  boltonvalley,
+  boreal,
+  bousquetmountain,
+  boynemountain,
+  breckenridge,
+  brettonwoods,
+  brianhead,
+  brighton,
+  bristol,
+  bromley,
+  buckhill,
+  burkemountain,
+  camdensnowbowl,
+  camelbackmountain,
+  cannonmountain,
+  cascademountain,
+  catamount,
+  chamonix,
+  cherrypeak,
+  china,
+  christiemountain,
+  cooper,
+  cooperspur,
+  copper,
+  copperpeak,
+  corbetscouloir,
+  cranmoremountain,
+  crestedbutte,
+  crotchedmountain,
+  crystalmountain,
+  crystalmtn,
+  dartmouthskiway,
+  deervalley,
+  dodge,
+  dollarmountain,
+  donner,
+  eaglecrest,
+  easton,
+  echo,
+  echovalley,
+  eldora,
+  eyak,
+  giantsridge,
+  gore,
+  granbyranch,
+  grandtarghee,
+  granitegorge,
+  granitepeak,
+  gunstockmountain,
+  harrishill,
+  losttrail,
+  louploup,
+  loveland,
+  lutsenmountains,
+  madriverglen,
+  magicmountain,
+  mammoth,
+  marquettemtn,
+  meanylodge,
+  megeve,
+  middleburysnowbowl,
+  missionridge,
+  monarch,
+  moose,
+  moritz,
+  mountbachelor,
+  mountkato,
+  mountpleasantedinboro,
+  mountrose,
+  mountsnow,
+  mountsunapee,
+  mtbaker,
+  mtbohemia,
+  mtbrighton,
+  mthoodmeadows,
+  mthoodskibowl,
+  nordicvalley,
+  northstar,
+  nubsnob,
+  okemo,
+  otisridge,
+  pajaritomountain,
+  palisadestahoe,
+  parkcity,
+  patspeak,
+  pebblecreek,
+  peek,
+  peter,
+  pico,
+  pinemtnjump,
+  plattekill,
+  pleasantmountain,
+  pomerelle,
+  powder,
+  powderhorn,
+  purgatory,
+  raggedmountain,
+  redriver,
+  roundtopmountain,
+  saddlebackmaine,
+  sandiapeak,
+  saskadenasix,
+  solitude,
+  spiritmountain,
+  steamboat,
+  stevenspass,
+  stowe,
+  stratton,
+  sugarbowl,
+  sugarbush,
+  sugarloaf,
+  summit,
+  summitsnoqualmie,
+  sundance,
+  sundayriver,
+  sunlight,
+  sunvalley,
+  tahoedonner,
+  tamarackresort,
+  tamarackskiarea,
+  taosskivalley,
+  telluride,
+  thunder,
+  timberlinelodge,
+  timberridge,
+  titcombmountain,
+  titus,
+  trappfamily,
+  trollhaugen,
+  tusseymountain,
+  tuxedo,
+  tyrolbasin,
+  vail,
+  valley,
+  verbier,
+  watervillevalley,
+  welchvillage,
+  west,
+  whaleback,
+  whiteface,
+  whitepass,
+  whitepine,
+  whitetailski,
+  wildcatmountain,
+  wildmountain,
+  willamettepass,
+  wilmotmountain,
+  windham,
+  winterpark,
+  wolfcreek,
+  wolfridgeski,
+  zermatt,
 };
